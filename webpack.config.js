d782fc608473e31d365b59af45abea6390d193e7
NICOLOSI Francesco <francesco.nicolosi@MacBookPro.lan>
2025-03-16 11:57:54 +0100

    Adding css folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './js/main.js',
@@ -11,6 +12,11 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './src/index.html',
             filename: 'index.html',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'css', to: 'css' },
+            ],
         }),
     ],
     module: {
